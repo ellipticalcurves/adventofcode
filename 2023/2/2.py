9ff3db5c6effbe47a51cd82bc02f52dc1ad598c8
Amin Akhtar <amin.akhtar19@imperial.ac.uk>
2023-12-03 02:10:30 +0000

    fin

--- 2023/2/2.py
+++ 2023/2/2.py
@@ -1,4 +1,4 @@
-input = "Game 11: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green"
+input = "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red"
 
 
 y = open( r"D:\AI\fruitshoppingdjango\adventofcode\adventofcode\2023\2\input.txt", "r")
@@ -8,9 +8,44 @@ y.close()
 print(inp[:4])
 x = input.split() # gives something like ['Game', '1:', '4', 'blue,', '4', 'red,', '16', 'green;', '14', 'green,', '5', 'red;', '1', 'blue,', '3', 'red,', '5', 'green']
 
+curr = x[1][:-1]
 
-# print(x)
-# print(x[1][:-1])
+
+def func2(x):
+    dic = {"b":0, "r":0, "g":0}
+
+    for i in range(2, len(x)):
+        if i % 2 == 0:
+            num = int(x[i])
+        elif i%2 == 1:
+            if dic[x[i][0]] < num:
+                dic[x[i][0]] = num
+    prodsum = 1
+    for i in dic.values():
+        prodsum*=i
+    return prodsum
+
+
+print(func2(x))
+
+cont = 0
+for i in range(len(inp)):
+    x = inp[i].split()
+    #print(x)
+    cont += func2(x)
+
+print(cont)
+
+        # if x[i][0]=="b" and num>14:
+        #     curr = 0
+        #     break
+        # elif x[i][0]=="g" and num>13:
+        #     curr = 0
+        #     break
+        # elif x[i][0]=="r"and num>12:
+        #     curr = 0
+        #     break
+print(int(curr))
 
 
 def func(x):
@@ -22,7 +57,6 @@ def func(x):
             if x[i][0]=="b" and num>14:
                 curr = 0
                 break
-
             elif x[i][0]=="g" and num>13:
                 curr = 0
                 break
@@ -31,12 +65,12 @@ def func(x):
                 break
     return int(curr)
 
-print(func(x))
+# print(func(x))
 
-cont = 0
-for i in range(len(inp)):
-    x = inp[i].split()
-    #print(x)
-    cont += func(x)
+# cont = 0
+# for i in range(len(inp)):
+#     x = inp[i].split()
+#     #print(x)
+#     cont += func(x)
 
-print(cont)
+# print(cont)
